9a924386389556e6213a39fd772a99574037c06e
pooja <poojaboppana99@gmail.com>
2025-06-02 22:13:14 +0530

    Expense Tracker

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -1,5 +1,6 @@
 
 let expenses= JSON.parse(localStorage.getItem("expenses")) || [];
+let totalexpense;
 function submitIncome(){
   let income=document.getElementById("income").value;
    localStorage.setItem("totalIncome",JSON.stringify(income));
@@ -7,16 +8,23 @@ display();
 }
 function submitExpense(){
    let expense=document.getElementById("expense").value;
-   expenses.push({amount: expense, date: new date().LocalString()});
-   localStorage.setItem("expenses",JSON.stringify(expense));
+   totalexpense +=expense;
+   expenses.push({amount: expense, date: new Date().toLocaleString()});
+   localStorage.setItem("expenses",JSON.stringify(expenses));
    display();
 }
 function display(){
      const income = JSON.parse(localStorage.getItem("totalIncome")) || 0;
       const incomeDiv = document.getElementById("incomeDisplay");
       incomeDiv.innerHTML = `Your Total Income is ${income}`;
-      expenses.forEach((expense,index)=>{
-        
-      })
+    
+        const expenseDisplay=document.getElementById('expenseDisplay');
+        expenseDisplay.innerHTML='';
+  expenses.forEach((expense,index)=>{
+   const expDiv = document.createElement("div");
+        expDiv.innerHTML = `On ${expense.date}, your expense is ${expense.amount}`;
+        expenseDisplay.appendChild(expDiv);
+
+      });
 }
 window.onload=display;
